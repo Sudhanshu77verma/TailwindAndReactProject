6a8ba2d45bd5b0dce33381bd95ff46de71e2655d
SudhanshuVerma <2003sudhanshucool@gmail.com>
2024-04-26 11:13:56 +0530

    make some changes in index.js

--- api/index.js
+++ api/index.js
@@ -3,23 +3,31 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import authRouter from './routes/auth.route.js'
 import cookieParser from "cookie-parser"
-
+import path from "path"
 dotenv.config()
 mongoose.connect(process.env.MONGO_DB_URL).then(()=>
 console.log("mongo db connected"))
 .catch((error)=>
 console.log(error))
 
+ const __dirname= path.resolve()
 
 const app =express()
 app.use(cookieParser());
 app.use(express.json())
+app.use('/api/auth',authRouter);
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
 })
 
-app.use('/api/auth',authRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+ 
 
 
 app.use((error,req,res,next)=>{
